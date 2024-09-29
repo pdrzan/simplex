7201e89fc2cd25004528613fe06a3e3d9cf8f58b
pdrzan <pedeozanelato19@gmail.com>
2024-09-29 15:30:52 -0400

    feature(simplex.c): add readInstance

--- src/simplex.c
+++ src/simplex.c
@@ -18,3 +18,44 @@ void notFeasibleBase()
     printf("V N\n");
     exit(0);
 }
+// Reads instance and stores information in n, m, c, i and r arrays
+void readInstance(char *instance_file_path, int *n, int *m, double **c, int **i, double ***r)
+{
+    int t, j;
+    FILE *instance_file;
+
+    instance_file = fopen(instance_file_path, "r");
+
+    if (instance_file == NULL)
+        notPossibleToBuildTableau();
+
+    fscanf(instance_file, "P %d %d", n, m);
+
+    *c = (double *)malloc(sizeof(double) * (*n + 1));
+    *i = (int *)malloc(sizeof(int) * (*m));
+    *r = (double **)malloc(sizeof(double *) * (*m));
+    for (j = 0; j < *m; j++)
+        (*r)[j] = (double *)malloc(sizeof(double) * (*n + 1));
+
+    fscanf(instance_file, "\nF");
+    for (t = 0; t < *n; t++)
+    {
+        fscanf(instance_file, "%lf", &(*c)[t]);
+        (*c)[t] *= -1;
+    }
+    c[*n + 1] = 0;
+
+    for (j = 0; j < *m; j++)
+    {
+        fscanf(instance_file, "\nR");
+
+        for (t = 0; t < *n + 1; t++)
+            fscanf(instance_file, "%lf", &(*r)[j][t]);
+
+        fscanf(instance_file, "\n");
+    }
+
+    fscanf(instance_file, "\nB");
+    for (j = 0; j < *m; j++)
+        fscanf(instance_file, "%d", &(*i)[j]);
+}
